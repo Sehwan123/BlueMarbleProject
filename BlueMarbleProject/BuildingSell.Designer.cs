fa1ea9b12d126d9e3814e0734e6d99554bf6a06e
Son Sewhan <sehwan4807@gmail.com>
2024-12-01 18:37:34 +0900

    건물 매각 코드 추가

---
+++ BlueMarbleProject/BuildingSell.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace BlueMarbleProject
+{
+    partial class BuildingSell
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "BuildingSell";
+        }
+
+        #endregion
+        private System.Windows.Forms.Label PayMoney;
+        private System.Windows.Forms.Label NowMoney;
+        private System.Windows.Forms.PictureBox Build;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+    }
+}
